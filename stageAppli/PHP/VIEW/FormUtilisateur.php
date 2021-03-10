03ddc4d786bb617938cbe8d28bac18ce4285007a
tancraft <tancraftwork@gmail.com>
2021-03-10 11:32:02 +0100

    10/03/2021 11:32:02,02

--- stageAppli/PHP/VIEW/FormUtilisateur.php
+++ stageAppli/PHP/VIEW/FormUtilisateur.php
@@ -4,13 +4,12 @@
     <?php
 
 $mode = $_GET['mode'];
-if (isset($_GET['id']))
-{
-    $idUser=$_GET['id'];
+if (isset($_GET['id'])) {
+    $idUser = $_GET['id'];
     $unUser = UtilisateursManager::findById($idUser);
-}else{
+} else {
     $unUser = new Utilisateurs();
-    $idUser= $unUser->getIdUser();
+    $idUser = $unUser->getIdUser();
 }
 $idRole = $unUser->getIdRole();
 switch ($mode) {
@@ -54,31 +53,31 @@ if ($mode == "ajouter") {
             <div class=" ">
                 <div class="info colonne ">
                     <label for="nomUser">Nom :</label>
-                    <input type="text" id="nom" <?= $disabled;?> name="nomUser" value="<?= $unUser->getNomUser();?>" required pattern="[a-zA-Z- ]{3,}">
+                    <input type="text" id="nom" <?=$disabled;?> name="nomUser" value="<?=$unUser->getNomUser();?>" required pattern="[a-zA-Z- ]{3,}">
                 </div>
                 <div class="info colonne ">
                     <label for="prenomUser">Prénom :</label>
-                    <input type="text" id="prenom" <?= $disabled;?> name="prenomUser" value="<?= $unUser->getPrenomUser();?>" required pattern="[a-zA-Z- ]{3,}">
+                    <input type="text" id="prenom" <?=$disabled;?> name="prenomUser" value="<?=$unUser->getPrenomUser();?>" required pattern="[a-zA-Z- ]{3,}">
                 </div>
             </div>
             <div>
 
                 <div class="info colonne  grande">
                     <label for="emailUser">Adresse E-mail :</label>
-                    <input type="text" id="email" <?= $disabled;?> name="emailUser" required
-                        pattern="^[0-9a-zA-Z._-]+@{1}[0-9a-zA-Z.-]{2,}[.]{1}[a-z]{2,6}$" value="<?= $unUser->getEmailUser();?>">
+                    <input type="text" id="email" <?=$disabled;?> name="emailUser" required
+                        pattern="^[0-9a-zA-Z._-]+@{1}[0-9a-zA-Z.-]{2,}[.]{1}[a-z]{2,6}$" value="<?=$unUser->getEmailUser();?>">
                 </div>
                 <div class="info colonne  grande">
                     <label for="telUser">Numéro de téléphone :</label>
-                    <input type="text" id="telephone" <?= $disabled;?> name="telUser" required
-                        pattern="^(01|02|03|04|05|06|07|08|09)[ \.\-]?[0-9]{2}[ \.\-]?[0-9]{2}[ \.\-]?[0-9]{2}[ \.\-]?[0-9]{2}" value="<?php echo $unUser->getTelUser();?>">
+                    <input type="text" id="telephone" <?=$disabled;?> name="telUser" required
+                        pattern="^(01|02|03|04|05|06|07|08|09)[ \.\-]?[0-9]{2}[ \.\-]?[0-9]{2}[ \.\-]?[0-9]{2}[ \.\-]?[0-9]{2}" value="<?php echo $unUser->getTelUser(); ?>">
                 </div>
             </div>
 
             <div>
                 <div class="info colonne center relatif">
                     <label for="mdpUser">Mot de passe :</label>
-                    <input type="password" id="mdp" <?= $disabled;?> name="mdpUser" value="<?= $unUser->getMdpUser();?>" required
+                    <input type="password" id="mdp" <?=$disabled;?> name="mdpUser" value="<?=$unUser->getMdpUser();?>" required
                         pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[!@#\$%\^&\*+])[a-zA-Z\d!@#\$%\^&\*+]{8,}$">
                     <div class="mini">
                         <div class="oeil">
@@ -118,7 +117,7 @@ if ($mode == "ajouter") {
 
                 <div class="info colonne center">
                     <label for="confirmation">Confirmation de mot de passe :</label>
-                    <input type="password" id="confirmation" <?= $disabled;?> name="confirmation" value="<?= $unUser->getMdpUser();?>" title="remettre le même mot de passe"
+                    <input type="password" id="confirmation" <?=$disabled;?> name="confirmation" value="<?=$unUser->getMdpUser();?>" title="remettre le même mot de passe"
                         required>
                 </div>
             </div>
@@ -127,9 +126,9 @@ if ($mode == "ajouter") {
                     <label for="idRole">Role :</label>
                     <?php
 
-$selRole = optionSelect($idUser,$idRole, "roles", "idRole", $mode, "libelleRole","");
-echo $selRole;
-?>
+                    $selRole = optionSelect($idUser, $idRole, "roles", "idRole", $mode, "libelleRole", "");
+                    echo $selRole;
+                    ?>
 
                 </div>
             </div>
@@ -160,7 +159,6 @@ switch ($mode) {
 echo '<div class="mini"></div>';
 echo '<a href="Index.php?page=listeUtilisateurs"><div class="bouton"><i class="far fa-arrow-alt-circle-left"></i>&nbsp Retour</div></a>';
 ?>
-
 
                    </div>
             </div>
